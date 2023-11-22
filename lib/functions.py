befff0dbe9059dc1eb81b906acbf1a407c0e1b73
chrisouko <oukodulo@gmail.com>
2023-11-22 04:51:25 -0800

    Completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
 def greet(name):
+    print("Hello, Guido!")
     pass
 
 def greet_with_default(name="programmer"):
+    print("Hello, programmer!")
+    pass
+
+def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
 def add(num1, num2):
+    result = 45 + 55
+    return result
     pass
 
 def halve(number):
+    result = number / 2.0
+    return result
     pass
